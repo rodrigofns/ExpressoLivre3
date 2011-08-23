fd671766f1363dddcff0965aa20997c7f30a907e
Filip Visic <visicfilip@gmail.com>
2011-08-23 09:45:32 +0200

    replace groupby with distinct

--- tine20/Tinebase/Tags.php
+++ tine20/Tinebase/Tags.php
@@ -407,9 +407,9 @@ class Tinebase_Tags
         $appId = Tinebase_Application::getInstance()->getApplicationByName($first->getApplication())->getId();
         
         $select = $this->_getSelect($recordIds, $appId);
-        $select->group(array('tagging.tag_id', 'tagging.record_id'));
+        $select->->distinct();
         Tinebase_Model_TagRight::applyAclSql($select, $_right, $this->_db->quoteIdentifier('tagging.tag_id'));
-        
+        if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . $select);
         $queryResult = $this->_db->fetchAll($select);
         //if (Tinebase_Core::isLogLevel(Zend_Log::DEBUG)) Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . ' ' . print_r($queryResult, TRUE));
         
