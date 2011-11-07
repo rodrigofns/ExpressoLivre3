b4cbe4539eac35647b4f579f9a8e788b39ff14ee
Flavio Gomes da Silva Lisboa <27524001878@serpro-1459235.pae.serpro>
2011-11-07 17:39:29 -0200

    added debug for recovered groups from ldap

--- tine20/Tinebase/Group/Ldap.php
+++ tine20/Tinebase/Group/Ldap.php
@@ -197,7 +197,9 @@ class Tinebase_Group_Ldap extends Tinebase_Group_Sql implements Tinebase_Group_I
             array('cn', 'description', $this->_groupUUIDAttribute)
         );
         
-        Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . 'getting groups from syncbackend... filter: ' . print_r($filter,true) . ' groupsDn: ' . $this->_options['groupsDn'] . 'groupSearchScope: ' . $this->_groupSearchScope . ' groupUUIDAttribute: ' . $this->_groupUUIDAttribute);        
+        Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . 'recovered groups from ldap: ' . print_r($groups));
+        
+        Tinebase_Core::getLogger()->debug(__METHOD__ . '::' . __LINE__ . 'getting groups from syncbackend... filter: ' . print_r($filter,true) . ' groupsDn: ' . $this->_options['groupsDn'] . ' groupSearchScope: ' . $this->_groupSearchScope . ' groupUUIDAttribute: ' . $this->_groupUUIDAttribute);        
                 
         $result = new Tinebase_Record_RecordSet('Tinebase_Model_Group');
         
