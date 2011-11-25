4198cb65624ab6862b6a27921cace54933bfaa58
Flávio Gomes da Silva Lisboa <flavio.lisboa@serpro.gov.br>
2011-11-25 09:54:46 -0200

    recovering of change in method create(), where there is necessary to explicit primary key for working in any database

--- tine20/Tinebase/Backend/Sql/Abstract.php
+++ tine20/Tinebase/Backend/Sql/Abstract.php
@@ -773,7 +773,7 @@ abstract class Tinebase_Backend_Sql_Abstract extends Tinebase_Backend_Abstract i
 		$this->_db->insert($this->_tablePrefix . $this->_tableName, $recordArray);
 
 		if (!$this->_hasHashId()) {
-			$newId = $this->_db->lastInsertId($this->getTablePrefix() . $this->getTableName());
+			$newId = $this->_db->lastInsertId($this->getTablePrefix() . $this->getTableName() ,$recordArray['id']);
 		}
 
 		// if we insert a record without an id, we need to get back one
