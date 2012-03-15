d6443c360a148255eac4151c5ee88e0e92eb8287
Flávio Gomes da Silva Lisboa <flavio.lisboa@serpro.gov.br>
2012-03-15 17:44:18 -0300

    Applied correct treatment for IN () into SQL SELECT

--- tine20/Calendar/Model/CalendarFilter.php
+++ tine20/Calendar/Model/CalendarFilter.php
@@ -44,10 +44,16 @@ class Calendar_Model_CalendarFilter extends Tinebase_Model_Filter_Container
         
         $quotedDisplayContainerIdentifier = $_backend->getAdapter()->quoteIdentifier('attendee.displaycontainer_id');
         
-        $_select->where($this->_getQuotedFieldName($_backend) . ' IN (?)', empty($this->_containerIds) ? " " : $this->_containerIds);
-        $_select->orWhere($quotedDisplayContainerIdentifier  .  ' IN (?)', empty($this->_containerIds) ? " " : $this->_containerIds);
+        $where = empty($this->_containerIds) ? 'FALSE' : 
+            $_select->getAdapter()->quoteInto($this->_getQuotedFieldName($_backend) . ' IN (?)', $this->_containerIds);
+        $orWhere = empty($this->_containerIds) ? 'FALSE' : 
+            $_select->getAdapter()->quoteInto($quotedDisplayContainerIdentifier  .  ' IN (?)', $this->_containerIds);
+        
+        $_select->where($where);
+        $_select->orWhere($orWhere);
+        
     }
-    
+        
     public function setRequiredGrants(array $_grants)
     {
         $this->_requiredGrants = $_grants;
