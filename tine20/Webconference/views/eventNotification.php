01133dc3ab79bb2221b7955384d61419376767c7
Marcelo Teixeira <marcelo.teixeira@serpro.gov.br>
2012-06-26 11:56:28 -0300

    Fixed message for correct translation.

--- tine20/Webconference/views/eventNotification.php
+++ tine20/Webconference/views/eventNotification.php
@@ -1,6 +1,6 @@
 <?php
 
-echo sprintf($this->translate->_('The user %1$s is inviting you to a webconference'), $this->fullUser->accountFullName) . ".\n";
+echo sprintf($this->translate->_('The user %s is inviting you to a Webconference'), $this->fullUser->accountFullName) . "\n";
 echo sprintf($this->translate->_('Use the link below to access the webconference')) . ".\n\n";
 echo $this->url;
 
