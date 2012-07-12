477965647eae300b5651707459e622c8e10fd93a
18072364049 <antonio-carlos.silva@serpro.gov.br>
2012-07-12 16:23:11 -0300

    Task #2075 - Fix Tine 2.0 in tag title by Expresso 3.0

--- tine20/Tinebase/views/jsclient.php
+++ tine20/Tinebase/views/jsclient.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <title>Tine 2.0</title>
+    <title>Expresso 3.0</title>  
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=8; IE=7" />
     <meta http-equiv="X-Tine20-Version" content="<?php echo TINE20_PACKAGESTRING ?>" />
