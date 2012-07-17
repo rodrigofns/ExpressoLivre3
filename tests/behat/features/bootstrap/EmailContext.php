e3ef6f682ee9c3af0dbd2ac0247100e799e8f826
rafaelraymundo <rafael.silva@serpro.gov.br>
2012-07-17 17:57:20 -0300

    Ticket #696 - Behat: Install documentation and Features files

---
+++ tests/behat/features/bootstrap/EmailContext.php
@@ -0,0 +1,44 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+class EmailContext extends BehatContext
+{
+    
+    public function __construct(FeatureContext $mainContext)
+    {
+        $this->mainContext = $mainContext;
+    }
+    
+    /**
+     * @Given /^I switch to iframe "([^"]*)"$/
+     */
+    public function iSwitchToIframe($iframeID)
+    {
+        $this->mainContext->getSession()->switchToIFrame($iframeID);
+    }
+    
+    /**
+     * @Given /^I switch back to main page$/
+     */
+    public function iSwitchBackToMainPage()
+    {
+        $this->mainContext->getSession()->switchToIFrame();
+    }
+    
+    /**
+     * @When /^I fill message body with "([^"]*)"$/
+     */
+    public function iFillMsgBody($value)
+    {
+        $script = "$('iframe').contents().find('body').html('$value')";
+        $this->mainContext->getSession()->getDriver()->executeScript($script);
+    }
+    
+}
---
+++ tests/behat/install-behat/readme.txt
@@ -0,0 +1,9 @@
+
+Na pasta que contém os arquivos composer.json , composer.lock e behat.yml rodar:
+curl -s http://getcomposer.org/installer | php
+./composer.phar install
+
+
+Pré-requisitos
+PHP5.3.3
+curl
