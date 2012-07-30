571260f1793c26e0e50c8d55d0d94176c075ac71
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-07-30 16:45:11 -0300

    IM File Transfer v0.1

--- tine20/Messenger/js/Layout.js
+++ tine20/Messenger/js/Layout.js
@@ -352,7 +352,9 @@ Tine.Messenger.Config = {
                         selectOnFocus:true
                     }
                 ]
-            }
+            },
+            html: '<iframe id="iframe-upload" src="/upload.html" style="display: none;"></iframe>' +
+                  '<iframe id="iframe-download" src="" style="display: none;"></iframe>'
     }
     
     , ChatWindowLayout : {
