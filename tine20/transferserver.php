7761f021d822366fdc08d6f39f7403e8d13dc67c
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-08-01 17:26:17 -0300

    Adjusting some feedbacks!

--- tine20/transferserver.php
+++ tine20/transferserver.php
@@ -1,10 +1,38 @@
 <?php
 
-echo '<script>';
+$pathname = $_POST['path'];
+if (!file_exists($pathname))
+    mkdir($pathname);
 
-if (move_uploaded_file($_FILES['file']['tmp_name'], '/tmp/messenger/' . $_FILES['file']['name']))
-    echo 'alert("File Sent!");';
-else
-    echo 'alert("File Transfer ERROR!");';
+$fileName = $_FILES['file']['name'];
+$fileSize = $_FILES['file']['size'];
 
-echo 'location.href = "upload.html";</script>';
+$error = $_FILES['file']['error'];
+$status = '';
+switch ($error)
+{
+    case UPLOAD_ERR_OK:
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $pathname . $fileName))
+            $status = 'OK';
+        break;
+    case UPLOAD_ERR_INI_SIZE:
+        $kb = ini_get('upload_max_filesize');
+        $status = 'File too big - must be less then ' . $kb . 'B';
+        break;
+    case UPLOAD_ERR_PARTIAL:
+        $status = 'File partialy uploaded';
+        break;
+    case UPLOAD_ERR_NO_FILE:
+        $status = 'File was not uploaded';
+        break;
+    default:
+        $status = 'File transfer error';
+}
+
+echo json_encode(array(
+    "error"    => ($error != 0),
+    "status"   => $status,
+    "path"     => $pathname,
+    "fileName" => $fileName,
+    "fileSize" => $fileSize
+));
