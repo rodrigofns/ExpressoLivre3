5d40a7ffb6046998f36090d4e435ace7d38eda37
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-08-01 17:26:17 -0300

    Adjusting some feedbacks!

--- tine20/download.php
+++ tine20/download.php
@@ -4,6 +4,7 @@ $file = urldecode($_GET['file']);
 $download = ($_GET['download'] == 'yes');
 
 if (file_exists($file) && $download) {
+    file_put_contents('/tmp/filetransfer.log', "GOT IN!!\n", FILE_APPEND);
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename='.basename($file));
@@ -15,7 +16,8 @@ if (file_exists($file) && $download) {
     ob_clean();
     flush();
     readfile($file);
+    file_put_contents('/tmp/filetransfer.log', "DOWNLOAD!!\n", FILE_APPEND);
 }
-
+file_put_contents('/tmp/filetransfer.log', "DELETING...\n", FILE_APPEND);
 unlink($file);
 exit;
