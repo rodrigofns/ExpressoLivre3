1276f20563c73021d1b1615fdce9a1a382e4bac0
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-08-03 09:20:10 -0300

    Removing debug lines!

--- tine20/Messenger/js/FileTransfer.js
+++ tine20/Messenger/js/FileTransfer.js
@@ -6,7 +6,7 @@ Tine.Messenger.FileTransfer = {
         var to = item.node.attributes.jid,
             iFrame = $('#iframe-upload'),
             inputFile = iFrame.contents().find('#sendfile');
-
+        
         inputFile.click();
         inputFile.one('change', function () {
             var form = $(this).parent('form');
