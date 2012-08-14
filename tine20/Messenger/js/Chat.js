9f41526a36d7c851316fc0266edbae661227f516
Bruno Ferraz Dourado <brferraz@hotmail.com>
2012-08-14 11:21:30 -0300

    Revert "It were created "Send File" and "Open Video" buttons"
    This reverts commit a647a767a47c4d062888f5dffa3d432c81599361.

--- tine20/Messenger/js/Chat.js
+++ tine20/Messenger/js/Chat.js
@@ -3,63 +3,7 @@ Ext.ns('Tine.Messenger');
 Tine.Messenger.Chat = Ext.extend(Ext.Window, {
     
     constructor: function () {
-        Ext.apply(this, {
-        iconCls:     'messenger-icon',
-        cls:         'messenger-chat-window',
-        width:       460,
-        minWidth:    400,
-        height:      360,
-        minHeight:   280,
-        closeAction: 'hide', //'close' - destroy the component
-        collapsible: true,
-        plain:       true,
-        layout:      'border',
-        tbar: {
-            items:[
-                {
-                    id: 'messenger-chat-options',
-                    text: Tine.Tinebase.appMgr.get('Messenger').i18n._('Options'),
-                    menu: {
-                            items:[{
-                                        id: 'messenger-chat-send',
-                                        icon: '/images/messenger/page_go.png',
-                                        text: Tine.Tinebase.appMgr.get('Messenger').i18n._('Send file'),
-                                        disabled: true,
-                                        handler: function(){
-                                            
-                                        }
-                                    },
-                                    {
-                                        id: 'messenger-chat-video',
-                                        icon: '/images/messenger/webcam.png',
-                                        text: Tine.Tinebase.appMgr.get('Messenger').i18n._('Start video chat'),
-                                        disabled: true,
-                                        handler: function() {
-                                            
-                                        }
-                                     },
-                                ]
-                        }
-                }]
-        },
-        listeners: {
-            beforerender: function(_box){
-                Tine.Messenger.AddItems(_box);
-            },
-            resize: function(_box, _width, _height){
-                Tine.Messenger.ChatHandler.adjustChatAreaHeight(_box.id, _width, _height);
-            },
-            show: function () {
-                this.setTextfieldFocus();
-            },
-            activate: function () {
-                this.setTextfieldFocus();
-            },
-            expand: function () {
-                this.setTextfieldFocus();
-            }
-        }
-  });
+        Ext.apply(this, Tine.Messenger.Config.ChatWindowLayout);
         Tine.Messenger.Chat.superclass.constructor.apply(this, arguments);
     },
     
