5236d1c3a53bbc4f0d51cafff1cbaccd3d520fae
Marcelo Teixeira <marcelo.teixeira@serpro.gov.br>
2012-08-30 11:53:19 -0300

    Fixed release number of Addressbook.

--- tine20/Addressbook/Setup/Update/Release5.php
+++ tine20/Addressbook/Setup/Update/Release5.php
@@ -230,6 +230,6 @@ class Addressbook_Setup_Update_Release5 extends Setup_Update_Abstract
         }
         
         $this->setTableVersion('addressbook', '17');
-        $this->setApplicationVersion('Addressbook', '6.0');
+        $this->setApplicationVersion('Addressbook', '5.7');
       }
 }
